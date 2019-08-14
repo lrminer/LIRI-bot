a3f73208f49386d0816c65be90e384e8d20bf163
Logan Miner <loganrobertminer@gmail.com>
2019-08-13 23:53:14 -0400

    fixed query string

--- LIRI.js
+++ LIRI.js
@@ -67,7 +67,15 @@ function searchOMDB() {
 
     axios.get('http://www.omdbapi.com/?apikey=trilogy&' + queryString.stringify(params))
         .then(function (response) {
-            console.log(response.data);
+            //console.log(response.data);
+            console.log("Title:" + response.data.Title);
+            console.log("Year:" + response.data.Year);
+            console.log("IMDB Rating:" + response.data.Ratings[0].value);
+            console.log("Rotten Tomatoes Rating:" + response.data.Ratings[1].value);
+            console.log("Country:" + response.data.Country);
+            console.log("Language:" + response.data.Language);
+            console.log("Plot:" + response.data.Plot);
+            console.log("Actors:" + response.data.Title);
         });
 
 
@@ -76,7 +84,7 @@ function searchOMDB() {
     // * Year the movie came out.
     // * IMDB Rating of the movie.
     // * Rotten Tomatoes Rating of the movie.
-    // * Country where the movie was produced.
+    // * Country where the movie was produced. 
     // * Language of the movie.
     // * Plot of the movie.
     // * Actors in the movie.
